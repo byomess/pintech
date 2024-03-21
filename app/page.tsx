1b55cd2a1daea5510b30cd085dff18b46c1416e2
Felipe Chierice <hello@felipechierice.dev>
2024-03-21 01:47:51 -0300

    fix: static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Head from 'next/head';
 import { ToolsProvider, Tool } from '@/hooks/useTools'
-import { getTools } from '@/api/pintech';
+// import { getTools } from '@/api/pintech';
 
 import { TagsSection } from './components/tags-section';
 import { ToolsSection } from './components/tools-section';
 
-const fetchTools = async (): Promise<Tool[]> => {
-  const tools: Tool[] = await getTools();
-  return tools;
-}
+import tools from '@/app/api/tools/data.json';
+
+// const fetchTools = async (): Promise<Tool[]> => {
+//   const tools: Tool[] = await getTools();
+//   return tools;
+// }
 
 const Home: React.FC = async () => {
-  const tools = await fetchTools();
+  // const tools = await fetchTools();
   return (
     <ToolsProvider tools={tools}>
       <div className="flex flex-col lg:flex-row bg-gray-800 text-white min-h-screen">
